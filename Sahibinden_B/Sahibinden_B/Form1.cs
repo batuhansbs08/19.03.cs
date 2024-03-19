a495dfba4bdd0dca1de562be3237f79f64aa6d34
batuhansbs08 <116164453+batuhansbs08@users.noreply.github.com>
2024-03-19 15:44:10 +0300

    Add files via upload

---
+++ Sahibinden_B/Sahibinden_B/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace Sahibinden_B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\ÖĞRENCİ\\Desktop\\Batuhan Subaşı\\Sahibinden_B\\AracBilgileri.mdb");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullanici = textBox1.Text;
+            string sifre = textBox2.Text;
+            baglan.Open();
+            OleDbCommand komut = new OleDbCommand("SELECT * FROM SistemKullanicilari WHERE KullaniciAdi= '" + kullanici + "' AND Sifre = '" + sifre + "'", baglan);
+            OleDbDataReader oku = komut.ExecuteReader();
+            if (oku.Read()) 
+            {
+                MessageBox.Show("Sistem Sayfasına Yönlendiriliyorsunuz...");
+                Form2 frm2 = new Form2();
+                this.Hide();
+                frm2.Show();
+            }
+            baglan.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox2.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/batuhansbs08");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://instagram.com/batu.iwnl_");
+        }
+    }
+}
---
+++ Sahibinden_B/Sahibinden_B/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sahibinden_B
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form3 frm3 = new Form3();
+            this.Hide();
+            frm3.Show();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("Çıkış Yapmak İstediğinizden Eminmisiniz?", "Uyarı", MessageBoxButtons.YesNo);
+            if (result1 == DialogResult.Yes)
+            {
+                Form1 frm1 = new Form1();
+                this.Close();
+                frm1.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Çıkış İşlemi Yapılmamıştır");
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Form4 frm4 = new Form4();
+            this.Hide();
+            frm4.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
